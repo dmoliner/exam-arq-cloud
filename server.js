b0d48b1ca38b6205ee4a3327da32ee696c96ccd7
muli <molinergd@gmail.com>
2026-05-29 16:30:57 +0200

    Refactor: optimitzar la lectura de blobs a Azure usant downloadToBuffer

--- server.js
+++ server.js
@@ -34,20 +34,8 @@ async function readQuestions(filename) {
             const blockBlobClient = containerClient.getBlockBlobClient(filename);
             const exists = await blockBlobClient.exists();
             if (exists) {
-                const downloadBlockBlobResponse = await blockBlobClient.download(0);
-                const streamToString = async (readableStream) => {
-                    return new Promise((resolve, reject) => {
-                        const chunks = [];
-                        readableStream.on("data", (data) => {
-                            chunks.push(data.toString());
-                        });
-                        readableStream.on("end", () => {
-                            resolve(chunks.join(""));
-                        });
-                        readableStream.on("error", reject);
-                    });
-                };
-                const dataStr = await streamToString(downloadBlockBlobResponse.readableStreamBody);
+                const buffer = await blockBlobClient.downloadToBuffer();
+                const dataStr = buffer.toString('utf8');
                 return JSON.parse(dataStr);
             } else {
                 console.warn(`[☁️ AZURE STORAGE] El blob ${filename} no existeix a Azure. Retornant array buit.`);
